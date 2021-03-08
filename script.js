9f02cdb6a2615d8dc582b7f95199027072cdfd5b
Andrew Ogilvie <primalorb@gmail.com>
2021-03-07 22:20:07 -0500

    refactor code

--- script.js
+++ script.js
@@ -1,60 +1,34 @@
 // Assignment code here
 function generatePassword() {
   var settings = {}
-  inputDigits( settings );
-  var conditions = ['lowercase', 'uppercase', 'numeric', 'special']
-  inputCondition( settings, conditions)
+  inputDigits( settings );;
+  var conditions = [ 'lowercase', 'uppercase', 'numeric', 'special' ];
+  inputCondition( settings, conditions );
  
   // loop over settings.length to build an empty array of length
   var passwordObj = []
   for ( var i = 0; i < settings.length; i++) {
     passwordObj[i] = ''
-  }
+  };
 
   // extract settings / values from the settings object
   var selectedConditons = Object.entries(settings)
+
   // empty object to push false value to
   var validate = []
-  var loop = 0
+
+  // set initial value for sequance of 'true' conditions in the loop
+  var sequence = 0
+
   // loop through selected conditions to check if they are selected. push false values into object
   for ( var i = 1; i < selectedConditons.length; i++ ) {
-    switch( selectedConditons[i][0] ){
-      case 'lowercase':
-        if( selectedConditons[i][1] ) {
-          loop = loop+1
-          selectedConditons[i][1] = loop
-          break 
-        }
-        validate.push( selectedConditons[i][1] )
-        break
-      case 'uppercase':
-        if( selectedConditons[i][1] ) {
-          loop = loop+1
-          selectedConditons[i][1] = loop
-          break 
-        }
-        validate.push( selectedConditons[i][1] )
-        break
-      case 'numeric':
-        if( selectedConditons[i][1] ) {
-          loop = loop+1
-          selectedConditons[i][1] = loop
-          break 
-        }
-        validate.push( selectedConditons[i][1] )
-        break
-      case 'special':
-        if( selectedConditons[i][1] ) {
-          loop = loop+1
-          selectedConditons[i][1] = loop
-          break 
-        }
-        validate.push( selectedConditons[i][1] )
-        break
-      default:
-        break
+    // if the value is true, increment the "sequence" value so that the final entry can be identified to add any remainder values too
+    if( selectedConditons[i][1] ) {
+      sequence = sequence + 1
+            selectedConditons[i][1] = sequence
     }
-  }
+  };
+
   // check validate object length (pushed false values) against initial conditions object. If the same, then user has selected all values as false and must re-initiate the function
   if ( validate.length === conditions.length ) {
     alert('You must select at least one condition')
@@ -62,80 +36,40 @@ function generatePassword() {
   };
 
   // equal allocation for each selection of the total length (take the input length, and divide by the total conditions - the false conditions)
-  var allocationEach =  Math.floor( settings.length / Math.floor( conditions.length - validate.length ))
+  var allocationEach =  Math.floor( settings.length / Math.floor( conditions.length - validate.length ) )
   // remainder to add to last selected condition
   var remainder = settings.length % ( conditions.length - validate.length )
 
   // loop through selected conditions update the generated password with random characters
   for ( var i = 1; i < selectedConditons.length; i++ ) {
-    switch( selectedConditons[i][0] ){
-      case 'lowercase':
-        if( selectedConditons[i][1] ) {
-          if(selectedConditons[i][1] < ( conditions.length - validate.length ) ) {
-            var low = 0 + ( allocationEach * ( selectedConditons[i][1] - 1 ) )
-            var high = allocationEach * ( selectedConditons[i][1] ) - 1
-            lowercaseInput( passwordObj, low, high )
-          } else {
-            var low = 0 + ( allocationEach * ( selectedConditons[i][1] - 1 ) )
-            var high = allocationEach * ( selectedConditons[i][1] ) -1 + remainder
-            lowercaseInput( passwordObj, low, high )
-         }
-          break 
-        }
-        break
-      case 'uppercase':
-        if( selectedConditons[i][1] ) {
-          if(selectedConditons[i][1] < ( conditions.length - validate.length ) ) {
-            var low = 0 + ( allocationEach * ( selectedConditons[i][1] - 1 ) )
-            var high = allocationEach * ( selectedConditons[i][1] ) - 1
-            uppercaseInput( passwordObj, low, high )
-          } else {
-            var low = 0 + ( allocationEach * ( selectedConditons[i][1] - 1 ) )
-            var high = allocationEach * ( selectedConditons[i][1] ) -1 + remainder
-            uppercaseInput( passwordObj, low, high )
-         }
-          break 
-        }
-        break
-      case 'numeric':
-        if( selectedConditons[i][1] ) {
-          if(selectedConditons[i][1] < ( conditions.length - validate.length ) ) {
-            var low = 0 + ( allocationEach * ( selectedConditons[i][1] - 1 ) )
-            var high = allocationEach * ( selectedConditons[i][1] ) - 1
-            numericInput( passwordObj, low, high )
-          } else {
-            var low = 0 + ( allocationEach * ( selectedConditons[i][1] - 1 ) )
-            var high = allocationEach * ( selectedConditons[i][1] ) -1 + remainder
-            numericInput( passwordObj, low, high )
-         }
-          break 
-        }
-        break
-      case 'special':
-        if( selectedConditons[i][1] ) {
-          if(selectedConditons[i][1] < ( conditions.length - validate.length ) ) {
-            var low = 0 + ( allocationEach * ( selectedConditons[i][1] - 1 ) )
-            var high = allocationEach * ( selectedConditons[i][1] ) - 1
-            specialInput( passwordObj, low, high )
-          } else {
-            var low = 0 + ( allocationEach * ( selectedConditons[i][1] - 1 ) )
-            var high = allocationEach * ( selectedConditons[i][1] ) -1 + remainder
-            specialInput( passwordObj, low, high )
-         }
-          break 
-        }
-        break
-      default:
-        break
+    // if number (truthy), as set by original loop
+    if( selectedConditons[i][1] ) {
+      var genObj = {
+        selected: selectedConditons[i][0],
+        series: selectedConditons[i][1],
+        maxSeries: ( conditions.length - validate.length ),
+        low: 0 + ( allocationEach * ( selectedConditons[i][1] - 1 ) ),
+        high: allocationEach * ( selectedConditons[i][1] ) - 1,
+        remainder: remainder,
+      }
+      handleInput( genObj, passwordObj )
     }
   }
+// after passwordObj has successfully been created in segments (result of 20 index, 4 selection would be "aaaaabbbbbcccccddddd"), shuffle the index 
 shuffle(passwordObj) ; 
+// after the shuffle, the same input could be "bdaccabbdabdbaacdcdc"
 
+//join and return the final password for posting to the DOM
 return passwordObj.join('')
-}
+};
 
+
+
+
+// function for input of length for the password
 function inputDigits(a) {
   let val = Number( prompt("How many characters would you like for your password? Enter a number between 8 - 128") )
+  //must be number, and must be between 8 and 128
   if ( !val || val < 8 || val > 128 ) {
     alert("Please enter a number between 8 - 128")
     return inputDigits();
@@ -143,6 +77,7 @@ function inputDigits(a) {
   a.length = val
 };
 
+// loop through input condition choices and post to settings object
 function inputCondition(a, b) {
   for( var i = 0; i < b.length; i++ ) {
     let val = confirm(`Do you want your password to include ${b[i]} characters?`)
@@ -150,46 +85,37 @@ function inputCondition(a, b) {
   }
 };
 
-function lowercaseInput(a, b, c) {
-  var chars = 'abcdefghijklmnopqrstuvwxyz'
-  for ( var i = b; i <= c; i++) {
-    a[i] = chars[Math.floor(Math.random() * (chars.length))]
+function handleInput( obj, outObj ) {
+  // object containing the type of available strings
+  var chars = {
+    lowercase: 'abcdefghijklmnopqrstuvwxyz',
+    uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    numeric: '1234567890',
+    special: '!@#$%^&*()_+~`|}{[]\:;?><,./-=',
+  };
+  // if the series is the last selection, apply any remainder to the value
+  if( obj.series === obj.maxSeries ) {
+    obj.high = obj.high + obj.remainder
+  }
+  // loop through between low and high indexes and set the values to a random selection from the chars string
+  for ( var i = obj.low; i <= obj.high; i++) {
+    outObj[i] = chars[`${obj.selected}`][ Math.floor(Math.random() * ( chars[`${obj.selected}`].length ) ) ]
   }
 }
 
-function uppercaseInput(a, b, c) {
-  var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-  for ( var i = b; i <= c; i++) {
-    a[i] = chars[Math.floor(Math.random() * (chars.length))]
-  }
-}
-
-function numericInput(a, b, c) {
-  var chars = '1234567890'
-  for ( var i = b; i <= c; i++) {
-    a[i] = chars[Math.floor(Math.random() * (chars.length))]
-  }
-}
-
-function specialInput(a, b, c) {
-  var chars = '!@#$%^&*()_+~`|}{[]\:;?><,./-='
-  for ( var i = b; i <= c; i++) {
-    a[i] = chars[Math.floor(Math.random() * (chars.length))]
-  }
-}
-
+// shuffle function
 function shuffle(a) {
   for ( var i = 0; i < a.length - 1; i++ ) {
+    // select a random number up to the length of the password
     var random = Math.floor( Math.random() * ( i ) )
+    // set aside the value of original indexed selection
     var orig = a[i]
+    // reset the value of the indexed selection to the value of a random index
     a[i] = a[random]
+    // set the value of the same random index to the value of the original indexed selection
     a[random] = orig
   }
-}
-
-
-
-
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -200,8 +126,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
-}
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
